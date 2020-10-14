3c273e2434301f56e0c331d9ae8ae85294f74716
Sidinei Silva <sidinei.silva02@gmail.com>
2020-10-14 15:20:28 -0300

    Importando o themeContainer

--- packages/client-plataform/pages/_app.tsx
+++ packages/client-plataform/pages/_app.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
+import ThemeContainer from '../contexts/theme/ThemeContainer';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeContainer>
+      <Component {...pageProps} />
+    </ThemeContainer>
+  );
 }
 
 export default MyApp;
