5bdad56abc8cf42e18d7d653715b7b4acd4d8d5a
Sidinei Silva <sidinei.silva02@gmail.com>
2020-10-15 14:58:21 -0300

    Instalando fonts 900

--- packages/client-plataform/pages/_document.tsx
+++ packages/client-plataform/pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document<DocumentProps> {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;200;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Overpass:wght@100;200;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" href="/favicon.png" />
