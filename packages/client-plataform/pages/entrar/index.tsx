4860d15dccf8e29473c003b42832149a7a10d795
Sidinei Silva <sidinei.silva02@gmail.com>
2020-10-17 11:42:17 -0300

    Criando pagina de login

---
+++ packages/client-plataform/pages/entrar/index.tsx
@@ -0,0 +1,63 @@
+import { Flex, Heading, Image, Grid } from '@chakra-ui/core';
+import React from 'react';
+
+import LandingLayout from '../../components/layouts/landingLayout';
+import FormSignIn from '../../components/ui/forms/signIn';
+
+export default function SignIn() {
+  return (
+    <LandingLayout>
+      <Grid
+        as="main"
+        w="100vw"
+        height="100vh"
+        alignItems="center"
+        templateColumns={{ lg: 'repeat(2, 1fr)' }}
+        backgroundImage="url('/img/bg3.png')"
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        paddingTop="110px"
+        paddingX={{ xxl: '300px' }}
+      >
+        <Flex
+          gridColumn={1}
+          display={{ base: 'none', lg: 'flex' }}
+          justifyContent="center"
+        >
+          <Image src="/svg/login-image.svg" w="100%" />
+        </Flex>
+
+        <Flex
+          gridColumn={{ md: 1, lg: 2 }}
+          borderRadius="md"
+          flexDir="column"
+          alignItems="center"
+        >
+          <Heading
+            fontSize={{ base: '4xl', lg: '48px', xl: '62px' }}
+            fontWeight="300"
+          >
+            Bem Vindo(a){' '}
+            <Heading
+              as="span"
+              fontSize={{ base: '4xl', lg: '48px', xl: '62px' }}
+              fontWeight="900"
+              color="blue.500"
+            >
+              Ninja
+            </Heading>
+            !
+          </Heading>
+          <Heading
+            fontSize={{ base: 'lg', lg: 'xl', xl: '28px' }}
+            fontWeight="700"
+          >
+            Estamos felizes com sua volta!
+          </Heading>
+          <FormSignIn mt={10} w={{ base: 'xs', lg: 'sm', xl: 'md' }} />
+        </Flex>
+      </Grid>
+    </LandingLayout>
+  );
+}
