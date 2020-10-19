bdb27e1dd10f6920fa85768e71582d58596e845d
Sidinei Silva <sidinei.silva02@gmail.com>
2020-10-19 19:57:19 -0300

    Criando pagina dashboard

---
+++ packages/client-plataform/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import DashboardLayout from '../../components/layouts/dashboardLayout';
+
+const Dashboard: React.FC = () => {
+  return (
+    <DashboardLayout>
+      <h1>Teste</h1>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
