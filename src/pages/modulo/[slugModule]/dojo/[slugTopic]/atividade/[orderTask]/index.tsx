80bdefefd34eaf5fd1e0b59054dcfac9191a5776
Sidinei Silva <sidinei.silva02@gmail.com>
2020-11-01 03:23:56 -0300

    Criando pagina de task

---
+++ src/pages/modulo/[slugModule]/dojo/[slugTopic]/atividade/[orderTask]/index.tsx
@@ -0,0 +1,121 @@
+import { Box, Heading, Grid, Text } from '@chakra-ui/core';
+import React from 'react';
+
+import DojoLayout from '../../../../../../../components/layouts/dojoLayout';
+import { getAllModules, getModuleBySlug } from '../../../../../../api/modules';
+import { getTaskBySlug } from '../../../../../../api/tasks';
+import {
+  getTopicBySlug,
+  getAllTopicsByModule
+} from '../../../../../../api/topics';
+
+interface Task {
+  title: string;
+  topic: string;
+  description: string;
+  order: number;
+  content: string;
+}
+
+interface Topic {
+  title: string;
+  slug: string;
+  description: string;
+  image: string;
+  order: number;
+  content: string;
+  tasks: Array<Task>;
+}
+
+interface Module {
+  title: string;
+  slug: string;
+  descriptionCard: string;
+  description: string;
+  image: string;
+}
+
+interface PageProps {
+  listTopics: Array<Topic>;
+  topic: Topic;
+  module: Module;
+  task: Task;
+}
+
+const DojoTask: React.FC<PageProps> = props => {
+  const { task, topic, module, listTopics } = props;
+
+  return (
+    <DojoLayout
+      moduleTitle={module.title}
+      moduleSlug={module.slug}
+      listTopics={listTopics}
+      topic={topic}
+    >
+      <Grid gap="1.75rem" maxWidth="75rem" minWidth={{ xl: '75rem' }}>
+        <Box maxWidth="43.75rem" textAlign="justify">
+          <Heading
+            fontSize={{ base: '2xl', lg: '28px' }}
+            fontWeight="700"
+            marginBottom="1.75rem"
+          >
+            {task.title}
+          </Heading>
+          <Text>{task.description}</Text>
+        </Box>
+        <Box />
+      </Grid>
+    </DojoLayout>
+  );
+};
+
+export default DojoTask;
+
+export async function getStaticProps(context: any) {
+  return {
+    props: {
+      listTopics: await getAllTopicsByModule(context.params.slugModule),
+      topic: await getTopicBySlug(
+        context.params.slugModule,
+        context.params.slugTopic
+      ),
+      module: await getModuleBySlug(context.params.slugModule),
+      task: await getTaskBySlug(
+        context.params.slugTopic,
+        context.params.orderTask
+      )
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const modules = await getAllModules();
+
+  const paths = await Promise.all(
+    modules.map(async module => {
+      const topics = await getAllTopicsByModule(module.slug);
+
+      const pathsTopics = topics.map(topic => {
+        const pathsTaks = topic.tasks.map(task => {
+          return {
+            params: {
+              orderTask: task.order.toString(),
+              slugTopic: topic.slug,
+              slugModule: module.slug
+            }
+          };
+        });
+        return pathsTaks;
+      });
+
+      return pathsTopics;
+    })
+  );
+
+  console.log(paths.flat(Infinity));
+
+  return {
+    paths: paths.flat(Infinity),
+    fallback: false
+  };
+}
