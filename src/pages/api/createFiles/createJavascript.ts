e06cef59adcb04da5f0ff6aebf6974f5be9a2b36
Sidinei Silva <sidinei.silva02@gmail.com>
2020-11-09 12:45:55 -0300

    Criando chamada api para criar aquivo JS

---
+++ src/pages/api/createFiles/createJavascript.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import { NextApiResponse, NextApiRequest } from 'next';
+import { resolve } from 'path';
+
+export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method !== 'POST') {
+    res.statusCode = 404;
+    res.json({
+      status: 'error',
+      message: 'Cannot GET /api/createFiles/createJavascript'
+    });
+    return;
+  }
+
+  const { code } = req.body;
+
+  const destination = resolve(
+    __dirname,
+    '..',
+    '..',
+    '..',
+    '..',
+    'tmp',
+    'javascriptTempCode'
+  );
+  const fileName = 'c2cd4f3a-229f-11eb-adc1-0242ac120002';
+
+  fs.writeFile(`./${destination}/${fileName}.js`, JSON.parse(code), err => {
+    if (err) console.info(err);
+  });
+
+  res.send(`${destination}/${fileName}.txt`);
+};
