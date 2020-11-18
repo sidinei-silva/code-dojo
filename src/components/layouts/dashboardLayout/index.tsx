f67547ed25635bbf45926dd01f95c5df268aa762
Sidinei Silva <sidinei.silva02@gmail.com>
2020-11-18 17:51:06 -0300

    Usando componente de Auth nos Layouts

--- src/components/layouts/dashboardLayout/index.tsx
+++ src/components/layouts/dashboardLayout/index.tsx
@@ -2,27 +2,30 @@ import { Flex, BoxProps } from '@chakra-ui/core';
 import React from 'react';
 
 import HeaderLoggedIn from '../../sections/header/loggedIn';
+import UserAuth from '../../utils/userAuth';
 
 const DashboardLayout: React.FC<BoxProps> = props => {
   const { children } = props;
 
   return (
-    <Flex direction="column" align="center" m="0 auto" {...props}>
-      <HeaderLoggedIn />
-      <Flex
-        backgroundImage="url('/img/bg3.png')"
-        backgroundSize="cover"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        paddingTop="110px"
-        paddingX={{ base: '25px', xxl: '300px' }}
-        w="100%"
-        justify="center"
-        minHeight="100vh"
-      >
-        {children}
+    <UserAuth>
+      <Flex direction="column" align="center" m="0 auto" {...props}>
+        <HeaderLoggedIn />
+        <Flex
+          backgroundImage="url('/img/bg3.png')"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          paddingTop="110px"
+          paddingX={{ base: '25px', xxl: '300px' }}
+          w="100%"
+          justify="center"
+          minHeight="100vh"
+        >
+          {children}
+        </Flex>
       </Flex>
-    </Flex>
+    </UserAuth>
   );
 };
 
