a183573296e01383ea1e6b415a56974881424287
Sidinei Silva <sidinei.silva02@gmail.com>
2020-11-18 12:40:30 -0300

    Criando regra de 2 consoles

--- _linters/introducao-a-logica-de-programacao-com-javascript/linter.ts
+++ _linters/introducao-a-logica-de-programacao-com-javascript/linter.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Linter } from 'eslint';
 
+import { getVariableByName } from '../utils';
+
 const linter = new Linter();
 
 linter.defineRule('var-length', {
@@ -16,6 +18,44 @@ linter.defineRule('var-length', {
       VariableDeclarator: node => {
         if (node.id.name.length < 2) {
           context.report(node, 'Variáveis tem que conter mais de 1 caractere');
+        }
+      }
+    };
+  }
+});
+
+linter.defineRule('contains-2-console', {
+  meta: {
+    type: 'task',
+    docs: {
+      description: 'Escreva um console.log()',
+      category: 'Check Task'
+    }
+  },
+  create(context) {
+    const isConsole = reference => {
+      const id = reference.identifier;
+
+      return id && id.name === 'console';
+    };
+
+    const isLog = reference => {
+      return reference.identifier.parent.property.name === 'log';
+    };
+
+    return {
+      'Program:exit': function (node) {
+        const scope = context.getScope();
+        const consoleVar = getVariableByName(scope, 'console');
+
+        const consoles = consoleVar
+          ? consoleVar.references
+          : scope.through.filter(isConsole);
+
+        const consolesLog = consoles.filter(isLog);
+
+        if (consolesLog.length < 2) {
+          context.report(node, 'Escreva um console.log()');
         }
       }
     };
