6446d5f224649f46a43f307e2439c47467ee4246
Sidinei Silva <sidinei.silva02@gmail.com>
2020-11-23 19:28:23 -0300

    Protegendo layout de dashboard

--- src/components/layouts/dashboardLayout/index.tsx
+++ src/components/layouts/dashboardLayout/index.tsx
@@ -2,27 +2,30 @@ import { Flex, BoxProps } from '@chakra-ui/core';
 import React from 'react';
 
 import HeaderLoggedIn from '../../sections/header/loggedIn';
+import ProtectRoute from '../../utils/ProtectRoute';
 
 const DashboardLayout: React.FC<BoxProps> = props => {
   const { children } = props;
 
   return (
-    <Flex direction="column" align="center" m="0 auto" {...props}>
-      <HeaderLoggedIn />
-      <Flex
-        backgroundImage="url('/img/bg3.png')"
-        backgroundSize="cover"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        paddingTop="110px"
-        paddingX={{ base: '25px', xxl: '300px' }}
-        w="100%"
-        justify="center"
-        minHeight="100vh"
-      >
-        {children}
+    <ProtectRoute>
+      <Flex direction="column" align="center" m="0 auto" {...props}>
+        <HeaderLoggedIn />
+        <Flex
+          backgroundImage="url('/img/bg3.png')"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+          paddingTop="110px"
+          paddingX={{ base: '25px', xxl: '300px' }}
+          w="100%"
+          justify="center"
+          minHeight="100vh"
+        >
+          {children}
+        </Flex>
       </Flex>
-    </Flex>
+    </ProtectRoute>
   );
 };
 
