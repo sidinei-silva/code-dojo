1c4acaac90a8e6fa57a3c14ea6b239771745b477
Sidinei Silva <sidinei.silva02@gmail.com>
2021-01-16 10:50:07 -0300

    Verificando se usuario existe no autenticated

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,28 @@
 import jwt from 'jsonwebtoken';
+import { MongoClient, Db, ObjectId } from 'mongodb';
 import { NextApiHandler, NextApiResponse, NextApiRequest } from 'next';
+import url from 'url';
+
+let cachedDb: Db = null;
+
+async function connectToDatabase(uri: string) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const dbName = url.parse(uri).pathname.substr(1);
+
+  const db = client.db(dbName);
+
+  cachedDb = db;
+
+  return db;
+}
 
 export interface requestCustom extends NextApiRequest {
   userId?: number;
@@ -20,6 +43,21 @@ export default (nextHandler: NextApiHandler) => async (
 
   try {
     const decoded: any = jwt.verify(token, process.env.API_APP_SECRET);
+
+    const dbUri = process.env.MONGODB_URI;
+    const db = await connectToDatabase(dbUri);
+    const collection = db.collection('users');
+
+    const checkEmailExist = await collection.findOne({
+      _id: new ObjectId(decoded._id),
+      email: decoded.email
+    });
+
+    if (!checkEmailExist) {
+      res.statusCode = 401;
+      return res.json({ status: 'error', message: 'Usuário não encontrado.' });
+    }
+
     req.userId = decoded._id;
     req.userEmail = decoded.email;
   } catch (err) {
